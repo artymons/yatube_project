21a8badd64a6f0c21edb18bc0bc8a6cf271033e0
Артемий Винн <hoztov25@ya.ru>
2022-04-07 14:39:45 +0300

    вся теория

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,15 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 # ice_cream/views.py
-from django.http import HttpResponse
+from .models import Post
+from .models import Group
+
 
 
 # Главная страница
-def index(request):    
-    return HttpResponse('Главная страница')
+def index(request):
+    # Одна строка вместо тысячи слов на SQL:
+    # в переменную posts будет сохранена выборка из 10 объектов модели Post,
+    # отсортированных по полю pub_date по убыванию (от больших значений к меньшим)
+    posts = Post.objects.order_by('-pub_date')[:10]
+    # В словаре context отправляем информацию в шаблон
+    title= 'Последние обновления на сайте'
+    context = {
+        'posts': posts,
+        'title': title
+    }
+    return render(request, 'posts/index.html', context)  
 
 
 # Страница со списком групп
 def group_posts(request, slug):
-    return HttpResponse(f'Посты, отфильтрованные по группам {slug}')
+    # Функция get_object_or_404 получает по заданным критериям объект 
+    # из базы данных или возвращает сообщение об ошибке, если объект не найден.
+    # В нашем случае в переменную group будут переданы объекты модели Group,
+    # поле slug у которых соответствует значению slug в запросе
+    group = get_object_or_404(Group, slug=slug)
+
+    # Метод .filter позволяет ограничить поиск по критериям.
+    # Это аналог добавления
+    # условия WHERE group_id = {group_id}
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    title = f'Записи сообщества {group}'
+    context = {
+        'group': group,
+        'posts': posts,
+        'title': title
+    }
+    return render(request, 'posts/group_list.html', context) 
